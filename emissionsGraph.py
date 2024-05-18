8453b843e4843dbbd587d4a0f35a491cf3af0008
dlw1rm4 <kaitlyn.loc101@gmail.com>
2024-05-18 14:51:19 -0700

    Delete table1.xlsx

--- emissionsGraph.py
+++ emissionsGraph.py
@@ -4,11 +4,13 @@ import pandas as pd
 
 app = Flask(__name__)
 
+df = pd.read_csv("table1.csv")
+df.to_csv("table1.csv", index=None)
 
 @app.route('/')
 def csvtohtml():
     data = pd.read_csv("table1.csv")
     return render_template('emissions.html',tables=[data.to_html()])
 
-if __name__ == "__main__":
+if __name__ == "main":
     app.run(host="localhost",port=int(5000))
