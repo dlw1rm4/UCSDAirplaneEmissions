3982534999f44b6805b8312a44bf922413e499c7
dlw1rm4 <kaitlyn.loc101@gmail.com>
2024-05-18 14:34:25 -0700

    Update emissions

--- emissionsGraph.py
+++ emissionsGraph.py
@@ -1,3 +1,16 @@
+from flask import Flask,render_template
+
 import pandas as pd
 
-carbon_emissions = pd.read_csv()
+app = Flask(name)
+
+df = pd.read_csv("table1.csv")
+df.to_csv("data.csv", index=None)
+
+@app.route('/')
+def csvtohtml():
+    data = pd.read_csv("table1.csv")
+    return render_template('emissions.html',tables=[data.to_html()])
+
+if name == "main":
+    app.run(host="localhost",port=int(5000))
